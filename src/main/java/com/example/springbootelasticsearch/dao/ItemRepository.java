e0fd72c20adee5221f69f51e5943748982794490
youxuehu <youxuehu@hotmail.com>
2019-07-30 22:09:28 +0800

    springboot elasticsearch

---
+++ src/main/java/com/example/springbootelasticsearch/dao/ItemRepository.java
@@ -0,0 +1,16 @@
+package com.example.springbootelasticsearch.dao;
+
+import com.example.springbootelasticsearch.entity.Item;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface ItemRepository  extends ElasticsearchRepository<Item,Long> {
+    /**
+     * @Description:根据价格区间查询
+     * @Param price1
+     * @Param price2
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    List<Item> findByPriceBetween(double price1, double price2);
+}
---
+++ src/test/java/com/example/springbootelasticsearch/SpringbootElasticsearchApplicationTests.java
@@ -0,0 +1,290 @@
+package com.example.springbootelasticsearch;
+
+import com.example.springbootelasticsearch.dao.ItemRepository;
+import com.example.springbootelasticsearch.entity.Item;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.metrics.avg.InternalAvg;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.FetchSourceFilter;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootElasticsearchApplicationTests {
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void contextLoads() {
+        elasticsearchTemplate.createIndex(Item.class);
+    }
+
+    /**
+     * @Description:定义新增方法
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void insert() {
+        for (int i = 1; i < 1000; i++) {
+            Item item = new Item(Long.valueOf(i), "小米手机7", " 手机",
+                    "小米", 3499.00, "http://image.baidu.com/13123.jpg");
+            itemRepository.save(item);
+        }
+    }
+
+    /**
+     * @Description:定义批量新增方法
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void insertList() {
+        List<Item> list = new ArrayList<>();
+        list.add(new Item(4L, "小米Mix2S", "手机", "小米", 4299.00, "http://image.baidu.com/13123.jpg"));
+        list.add(new Item(5L, "荣耀V10", "手机", "华为", 2799.00, "http://image.baidu.com/13123.jpg"));
+       // 接收对象集合，实现批量新增
+        itemRepository.saveAll(list);
+    }
+
+    /**
+     * @Description:定义查询方法,含对价格的降序、升序查询
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void testQueryAll(){
+        // 查找所有
+        //Iterable<Item> list = this.itemRepository.findAll();
+        // 对某字段排序查找所有 Sort.by("price").descending() 降序
+        // Sort.by("price").ascending():升序
+        Iterable<Item> list = this.itemRepository.findAll(Sort.by("price").ascending());
+
+        for (Item item:list){
+            System.out.println(item);
+        }
+    }
+
+    /**
+     * @Description:按照价格区间查询
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void queryByPriceBetween(){
+        List<Item> list = this.itemRepository.findByPriceBetween(2000.00, 3500.00);
+        for (Item item : list) {
+            System.out.println("item = " + item);
+        }
+    }
+
+    /**
+     * @Description:matchQuery底层采用的是词条匹配查询
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void testMatchQuery(){
+        // 构建查询条件
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 添加基本分词查询
+        queryBuilder.withQuery(QueryBuilders.matchQuery("title", "小米手机"));
+        // 搜索，获取结果
+        Page<Item> items = this.itemRepository.search(queryBuilder.build());
+        // 总条数
+        long total = items.getTotalElements();
+        System.out.println("total = " + total);
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    /**
+     * @Description:
+     * termQuery:功能更强大，除了匹配字符串以外，还可以匹配
+     * int/long/double/float/....
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void testTermQuery(){
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        builder.withQuery(QueryBuilders.termQuery("price",998.0));
+        // 查找
+        Page<Item> page = this.itemRepository.search(builder.build());
+
+        for(Item item:page){
+            System.out.println(item);
+        }
+    }
+
+    /**
+     * @Description:布尔查询
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void testBooleanQuery(){
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+
+        builder.withQuery(
+                QueryBuilders.boolQuery().must(QueryBuilders.matchQuery("title","华为"))
+                        .must(QueryBuilders.matchQuery("brand","华为"))
+        );
+
+        // 查找
+        Page<Item> page = this.itemRepository.search(builder.build());
+        for(Item item:page){
+            System.out.println(item);
+        }
+    }
+
+
+    /**
+     * @Description:模糊查询
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void testFuzzyQuery(){
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        builder.withQuery(QueryBuilders.fuzzyQuery("title","faceoooo"));
+        Page<Item> page = this.itemRepository.search(builder.build());
+        for(Item item:page){
+            System.out.println(item);
+        }
+
+    }
+
+    /**
+     * @Description:分页查询
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void searchByPage(){
+        // 构建查询条件
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 添加基本分词查询
+        queryBuilder.withQuery(QueryBuilders.termQuery("category", "小米手机7"));
+        // 分页：
+        int page = 0;
+        int size = 2;
+        queryBuilder.withPageable(PageRequest.of(page,size));
+
+        // 搜索，获取结果
+        Page<Item> items = this.itemRepository.search(queryBuilder.build());
+        // 总条数
+        long total = items.getTotalElements();
+        System.out.println("总条数 = " + total);
+        // 总页数
+        System.out.println("总页数 = " + items.getTotalPages());
+        // 当前页
+        System.out.println("当前页：" + items.getNumber());
+        // 每页大小
+        System.out.println("每页大小：" + items.getSize());
+
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    /**
+     * @Description:排序查询
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void searchAndSort(){
+        // 构建查询条件
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 添加基本分词查询
+        queryBuilder.withQuery(QueryBuilders.termQuery("category", "手机"));
+
+        // 排序
+        queryBuilder.withSort(SortBuilders.fieldSort("price").order(SortOrder.ASC));
+
+        // 搜索，获取结果
+        Page<Item> items = this.itemRepository.search(queryBuilder.build());
+        // 总条数
+        long total = items.getTotalElements();
+        System.out.println("总条数 = " + total);
+
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    /**
+     * @Description:按照品牌brand进行分组
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void testAgg(){
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 不查询任何结果
+        queryBuilder.withSourceFilter(new FetchSourceFilter(new String[]{""}, null));
+        // 1、添加一个新的聚合，聚合类型为terms，聚合名称为brands，聚合字段为brand
+        queryBuilder.addAggregation(
+                AggregationBuilders.terms("brands").field("brand"));
+        // 2、查询,需要把结果强转为AggregatedPage类型
+        AggregatedPage<Item> aggPage = (AggregatedPage<Item>) this.itemRepository.search(queryBuilder.build());
+        // 3、解析
+        // 3.1、从结果中取出名为brands的那个聚合，
+        // 因为是利用String类型字段来进行的term聚合，所以结果要强转为StringTerm类型
+        StringTerms agg = (StringTerms) aggPage.getAggregation("brands");
+        // 3.2、获取桶
+        List<StringTerms.Bucket> buckets = agg.getBuckets();
+        // 3.3、遍历
+        for (StringTerms.Bucket bucket : buckets) {
+            // 3.4、获取桶中的key，即品牌名称
+            System.out.println(bucket.getKeyAsString());
+            // 3.5、获取桶中的文档数量
+            System.out.println(bucket.getDocCount());
+        }
+
+    }
+
+    /**
+     * @Description:嵌套聚合，求平均值
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    @Test
+    public void testSubAgg(){
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 不查询任何结果
+        queryBuilder.withSourceFilter(new FetchSourceFilter(new String[]{""}, null));
+        // 1、添加一个新的聚合，聚合类型为terms，聚合名称为brands，聚合字段为brand
+        queryBuilder.addAggregation(
+                AggregationBuilders.terms("brands").field("brand")
+                        .subAggregation(AggregationBuilders.avg("priceAvg").field("price")) // 在品牌聚合桶内进行嵌套聚合，求平均值
+        );
+        // 2、查询,需要把结果强转为AggregatedPage类型
+        AggregatedPage<Item> aggPage = (AggregatedPage<Item>) this.itemRepository.search(queryBuilder.build());
+        // 3、解析
+        // 3.1、从结果中取出名为brands的那个聚合，
+        // 因为是利用String类型字段来进行的term聚合，所以结果要强转为StringTerm类型
+        StringTerms agg = (StringTerms) aggPage.getAggregation("brands");
+        // 3.2、获取桶
+        List<StringTerms.Bucket> buckets = agg.getBuckets();
+        // 3.3、遍历
+        for (StringTerms.Bucket bucket : buckets) {
+            // 3.4、获取桶中的key，即品牌名称  3.5、获取桶中的文档数量
+            System.out.println(bucket.getKeyAsString() + "，共" + bucket.getDocCount() + "台");
+
+            // 3.6.获取子聚合结果：
+            InternalAvg avg = (InternalAvg) bucket.getAggregations().asMap().get("priceAvg");
+            System.out.println("平均售价：" + avg.getValue());
+        }
+
+    }
+}
